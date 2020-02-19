0309de04efa861a00db7b42becccc213b6fd44a2
Hykilpikonna <admin@moecraft.cc>
2020-02-19 17:36:38 -0500

    [+] Create utility to print message

---
+++ src/main/java/org/hydev/hyritone/MiscUtils.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.hydev.hyritone;
+
+import net.minecraft.util.text.StringTextComponent;
+
+import static baritone.api.utils.Helper.mc;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2020-02-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-02-19 13:28
+ */
+public class MiscUtils
+{
+    public static void print(String text)
+    {
+        if (mc.player == null) return;
+
+        mc.player.sendMessage(new StringTextComponent(text));
+        System.out.println("Log: " + text);
+    }
+
+    public static void debug(String text)
+    {
+        print("§8[§7Debug§8]§7 " + text);
+    }
+}
